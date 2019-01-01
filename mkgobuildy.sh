c03d5cdff8dded22cf0a6f2b17bab3d284bc47d6
Ikey Doherty <ikey.doherty@intel.com>
2019-01-01 14:46:22 +0000

    mkgobuildy: Clean up and reorganise
    Signed-off-by: Ikey Doherty <ikey.doherty@intel.com>

--- mkgobuildy.sh
+++ mkgobuildy.sh
@@ -1,13 +1,29 @@
 #!/bin/bash
 set -e
 
+DEFAULT_ARGS="-Dwith-libressl-mode=shim -Dwith-new-libcxx-abi=true"
+
 if [[ ! -d buildroot/build32 ]]; then
     ASFLAGS="$ASFLAGS --32" CFLAGS="$CFLAGS -m32" CXXFLAGS="$CXXFLAGS -m32" LDFLAGS="$LDFLAGS -m32" \
-    meson buildroot/build32 -Dwith-libressl-mode=shim --prefix=/usr --sysconfdir=/etc --datadir=/usr/share --libdir=/usr/lib32 -Dwith-shim=none -Dwith-new-libcxx-abi=true --buildtype debugoptimized
+    meson buildroot/build32 $DEFAULT_ARGS \
+        --prefix=/usr \
+        --sysconfdir=/etc \
+        --datadir=/usr/share \
+        --libdir=/usr/lib32 \
+        -Dwith-shim=none \
+        --buildtype debugoptimized
 fi
 
 if [[ ! -d buildroot/build64 ]]; then
-    meson buildroot/build64 -Dwith-libressl-mode=shim --prefix=/usr --sysconfdir=/etc --datadir=/usr/share --libdir=/usr/lib64 -Dwith-shim=replacement -Dwith-steam-binary=/usr/lib64/steam/steam -Dwith-frontend=true -Dwith-new-libcxx-abi=true --buildtype debugoptimized
+    meson buildroot/build64 $DEFAULT_ARGS \
+        --prefix=/usr \
+        --sysconfdir=/etc \
+        --datadir=/usr/share \
+        --libdir=/usr/lib64 \
+        -Dwith-shim=replacement \
+        -Dwith-steam-binary=/usr/lib64/steam/steam \
+        -Dwith-frontend=true \
+        --buildtype debugoptimized
 fi
 
 ninja -C buildroot/build32
